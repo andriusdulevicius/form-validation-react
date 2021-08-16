7c134cb98a5d0da11bb5935d436504b5f98975a5
AndriusDulevicius <andriusdulevicius@email.com>
2021-08-16 13:44:21 +0300

    using ref sample

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,26 @@
+import { useState, useRef } from 'react';
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
+  const nameChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+  const formSubmissionhandler = (e) => {
+    e.preventDefault();
+    console.log('form submission in progress');
+    // naudojant ref gauti ivesties lauko reiksme
+    // jis naudojamas retais atvejais, nerekomenduojama updatinti dom per ref
+    const enteredValue = nameInputRef.current.value;
+    console.log('value using ref: ', enteredValue);
+    setEnteredName('');
+  };
   return (
-    <form>
+    <form onSubmit={formSubmissionhandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} onChange={nameChangeHandler} type='text' id='name' value={enteredName} />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
